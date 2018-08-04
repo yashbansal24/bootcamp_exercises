6f6e6ff85c40c8f4c6ec1a9aca914aba04195b78
Singh <gsingh4@lm-maa-22017423.paypalcorp.com>
2018-08-04 11:42:52 +0530

    create dict

---
+++ dictimport.js
@@ -0,0 +1,28 @@
+function create_dict(list,input,out)
+{
+    let map={};
+    for(var i=0;i<list.length;i++){
+    	if(list[i].length==input.length)
+    		map[list[i]]=1;
+    }
+    return map;
+}
+
+function read_dict(filename){
+	var fs=require('fs');
+	var list=fs.readFileSync(filename,'utf-8');
+	list=list.split('\r\n');
+	return list
+}
+
+
+
+function return_dict(input,out){
+	list = read_dict("sowpods.txt")
+
+	if(input.length != out.length)
+		console.log("invalid")
+	return create_dict(list,input,out);
+}
+
+console.log(return_dict("clay","gold"));
