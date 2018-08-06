26fc515cc32c01bf143b72e6a0120c012fac3ce1
Anirud Samala <anirud.samala+github@gmail.com>
2018-08-06 17:49:53 +0530

    index: added constraints checks

--- scrabble/index.js
+++ scrabble/index.js
@@ -7,9 +7,14 @@ const dict = dictmodule.return_dict();
 
 function passesConstraints(word,rackConstraints) {
   for(let constraint of rackConstraints) {
-    //console.log(constraint);
-    if(word.indexOf(constraint.second) - word.indexOf(constraint.first) !== constraint.diff)
-      return false;
+    if(constraint.second) {
+      //console.log(constraint);
+      if(word.indexOf(constraint.second) - word.indexOf(constraint.first) !== constraint.diff)
+        return false;
+    }
+    else if(constraint.first) {
+      return (word.indexOf(constraint.first) !== -1);
+    }
   }
   return true;
 }
@@ -25,13 +30,6 @@ function getHighestWord(rack,constraints) {
   //console.log(allowedWords);
   return findMaxScore.getMaxScoreWord(allowedWords,rack);
 }
-
-let sampleRack2 = [
-  {letter:'M', pos:-1},
-  {letter:'E', pos:2},
-  {letter:'S', pos:-1},
-  {letter:'S', pos:-1}
-];
 
 let sampleInputs = [
   {
