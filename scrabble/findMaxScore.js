dda07271d29b1d91a422b4288c22ca0bc8631288
schandpatro <swadhinchandpatro@gmail.com>
2018-08-06 16:43:26 +0530

    findMaxScore: added rack param

--- scrabble/findMaxScore.js
+++ scrabble/findMaxScore.js
@@ -1,13 +1,17 @@
 const wordScore = require('./scorecalc');
 
-function getMaxScoreWord(legalWords) {
-    let maxWord = {'word':'','score':-1};
+function getMaxScoreWord(legalWords,rack) {
+    let maxWord = [];
+    let maxScore = -1;
     for (let word of legalWords){
-        let calcWord = wordScore.wordscore(word);
+        let calcWord = wordScore.wordscore(word,rack);
 
-        if ( calcWord.score > maxWord.score){
-            maxWord.word=word;
-            maxWord.score = calcWord.score;
+        if ( calcWord.score > maxScore){
+            maxScore = calcWord.score;
+            maxWord = [];
+            maxWord.push({'word':word,'score':calcWord.score});
+        }else if(calcWord.score == maxScore){
+            maxWord.push({'word':word,'score':calcWord.score});
         }
     }
     return maxWord;
