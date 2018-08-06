06c6fa597593ada3fbb6d5f3734c4e91e1ffbbb5
Anirud Samala <anirud.samala+github@gmail.com>
2018-08-06 14:44:14 +0530

    added index file

---
+++ scrabble/index.js
@@ -0,0 +1,23 @@
+const dictmodule = require('./dictimport');
+const wordGen = require('./wordGenerator');
+const scoreCalc = require('./scorecalc');
+const findMaxScore = require('./findMaxScore');
+
+function getHighestWord(rack) {
+  let possibleWords = wordGen.genPossibleWordsFromRack(rack);
+  let legalWords = [];
+  let dict = dictmodule.return_dict();
+  for(let word of possibleWords) {
+    if(dictmodule.check_if_present(dict,word)) {
+      legalWords.push(word);
+    }
+  }
+
+  return findMaxScore.getMaxScoreWord(legalWords);
+}
+
+function main() {
+  console.log('yes');
+}
+
+main();
