c5aabae18201a45b345c952def820febb710c10b
Anirud Samala <anirud.samala+github@gmail.com>
2018-08-09 13:32:14 +0530

    odometer: added init function

--- odometer/index.js
+++ odometer/index.js
@@ -3,7 +3,7 @@ function genOdometerValues(n) {
     return [];
   if(n === 1)
     return [1,2,3,4,5,6,7,8,9];
-  let tmpList = genOdometerUtil(n-1);
+  let tmpList = genOdometerValues(n-1);
   let res = [];
   for(let num of tmpList) {
     for(let i=(num%10)+1;i<=9;++i)
@@ -12,16 +12,24 @@ function genOdometerValues(n) {
   return res;
 }
 
+function initOdometer() {
+  if(this.odometerValues.length > 0)
+    this.currentValue = this.odometerValues[0];
+}
+
 class Odometer {
   constructor(n) {
     this.odometerValues = genOdometerValues(n);
-    if(odometerValues.length > 0)
-      this.currentValue = odometerValues[0];
+    this.currentValue = 0;
 
     this.nextValue = nextValue;
     this.prevValue = prevValue;
     this.nextNthValue = nextNthValue;
     this.prevNthValue = prevNthValue;
     this.diff = diff;
+
+    initOdometer();
   }
 }
+
+console.log(genOdometerValues(3).toString());
